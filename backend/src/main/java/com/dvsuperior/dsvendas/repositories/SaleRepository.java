899af80bffe12d0f5fdd6103a11f6ecddbcaa441
renatomak <renato.mark.silva@gmail.com>
2021-09-10 11:07:03 -0300

    Pagination

---
+++ backend/src/main/java/com/dvsuperior/dsvendas/repositories/SaleRepository.java
@@ -0,0 +1,10 @@
+package com.dvsuperior.dsvendas.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.dvsuperior.dsvendas.entities.Sale;
+
+
+public interface SaleRepository extends JpaRepository<Sale, Long>{
+
+}
