7f329dbb3fe9e1778f5109278443cb3ed3419851
renatomak <renato.mark.silva@gmail.com>
2021-09-11 21:17:29 -0300

    DataTable integration

--- frontend/src/types/sale.ts
+++ frontend/src/types/sale.ts
@@ -1,10 +1,35 @@
+import { Seller } from "./seller"
+
+
+export type Sale = {
+    id: number;
+    visited: number;
+    deals: number;
+    amount: number;
+    date: string;
+    seller: Seller;
+}
+
+export type SalePage = {
+    content?: Sale[];
+    last: boolean;
+    totalPages: number;
+    totalElements: number;
+    first: boolean;
+    size?: number;
+    number: number;
+    numberOfElements?: number;
+    empty?: boolean;
+}
+
 export type SaleSum = {
-    sellerName: string,
-    sum: number,
+    sellerName: string;
+    sum: number;
 }
 
 export type SaleSuccess = {
-    sellerName: string,
-    visited: number,
-    deals: number,
+    sellerName: string;
+    visited: number;
+    deals: number;
 }
+
